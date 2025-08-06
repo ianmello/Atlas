9f4bedb679608fdf43f320e87d9ac09c8ab6f8ce
Bruno Dantas <brunodantassp0@gmail.com>
2025-08-05 21:36:16 -0300

    add-painel

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/brunodantas/Documents/faculdade/tcc/Atlas/venv"
+set -gx VIRTUAL_ENV "/Users/brunodantas/Documents/faculdade/Atlas/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
